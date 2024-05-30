b7cbc0d8d68ef92a607190358012f86e51258156
Student gauravyadav <gauravyadav2003.gy@gmail.com>
2024-05-30 22:59:25 +0530

    define schema and connect database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express= require("express");
 const router=require("./router/auth-router.js")
 const app=express();
+const connectDb=require("./utils/db.js");
 
 app.use(express.json());//parsing json data from requests ,middleware
 
@@ -11,6 +13,10 @@ app.use("/api/auth",router)  // mount path
 
 const PORT=8000;
 
+connectDb().then(()=>{
+
 app.listen(PORT,()=>{
     console.log(`Server is running at port ${PORT}`);
+});
+ 
 });
